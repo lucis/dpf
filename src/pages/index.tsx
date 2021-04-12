dd59f4054ce330f79fa2d532aaa87445feb01c46
Luciano Júnior <luciannojunior@gmail.com>
2021-04-12 18:20:21 -0300

    Moving to TS

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Capítulo "Deus, Pátria e Família"</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
