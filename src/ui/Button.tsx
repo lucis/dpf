06960f9440bf6774776366dc2bf6d0e899386129
Luciano Júnior <luciannojunior@gmail.com>
2021-04-14 12:07:43 -0300

    Trying to commit something unformatted

--- src/ui/Button.tsx
+++ src/ui/Button.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
 import type { FC } from 'react'
 
-type  Props = {
+type Props = {
   type: 'primary' | 'secondary'
 }
 
-const Button: FC<Props> = ({ type, children }) => {
+const  Button: FC<Props> = ({ type, children }) => {
   return <button className="bg-amarelo p-2 rounded">{children}</button>
 }
 
