91da2f07833dd844998af433c1b27b466214f077
Lucis <luciannojunior@gmail.com>
2021-04-14 12:24:00 -0300

    Adding Lato font and commit hooks (#2)
    * Adding font Lato
    
    * Running prettier on files
    
    * Adding Lato font to CSS
    
    * Adding husky to automate formatting
    
    * Trying to commit something unformatted
    
    * Fixing package.json
    
    * Trying to commit something unformatted
    
    * Trying to commit something unformatted
    
    * Trying to commit something unformatted
    
    * Trying to commit something unformatted
    
    * Trying to commit something unformatted
    
    * Trying to commit something unformatted
    
    * Lint stage works
    
    * Trying to install Husky again
    
    * Error
    
    * Trying to add husky again:

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,20 +1,22 @@
-import { action } from "@storybook/addon-actions"
+import { action } from '@storybook/addon-actions'
 
-import '../src/styles/global.css';
+import '@fontsource/lato/latin-400.css'
+import '@fontsource/lato/latin-400.css'
+import '../src/styles/global.css'
 
 global.___loader = {
   enqueue: () => {},
   hovering: () => {},
 }
 
-global.__BASE_PATH__ = "/"
+global.__BASE_PATH__ = '/'
 
-window.___navigate = pathname => {
-  action("NavigateTo:")(pathname)
+window.___navigate = (pathname) => {
+  action('NavigateTo:')(pathname)
 }
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,3 @@
- import './src/styles/global.css';
+import '@fontsource/lato/latin-400.css'
+import '@fontsource/lato/latin-400.css'
+import './src/styles/global.css'
